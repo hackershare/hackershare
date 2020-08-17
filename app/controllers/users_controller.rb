d81697623e76060d408a87f8a54c8a726b447dc1
Hooopo Wang <hoooopo@gmail.com>
2020-08-17 18:48:54 +0800

    update footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:hover]
+  skip_before_action :authenticate_user!, only: [:hover, :show]
 
   def show
     @user = User.find(params[:id])
