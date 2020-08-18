9b4941db3484415c2d92de1519b0f5e8ecccc383
Hooopo Wang <hoooopo@gmail.com>
2020-08-19 03:35:56 +0800

    link storage dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ Rake::Task["deploy:assets:backup_manifest"].clear_actions
 append :linked_files, ".env"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads", "storage"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
