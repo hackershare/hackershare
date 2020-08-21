d5e403b8d6aa0df53ba4aa1a0cb5b0c73564d58f
Hooopo Wang <hoooopo@gmail.com>
2020-08-21 17:10:38 +0800

    fix index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:hover, :show]
+  skip_before_action :authenticate_user!, only: [:hover, :show, :index]
 
   def show
     @user = User.find(params[:id])
