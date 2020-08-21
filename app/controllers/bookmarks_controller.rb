7a77ea757ef365f9ae20f988cd7e46abb4362c37
Hooopo Wang <hoooopo@gmail.com>
2020-08-21 20:44:35 +0800

    fix before action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  skip_before_action :authenticate_user!, only: %i[index show]
   def new; end
 
   def index
