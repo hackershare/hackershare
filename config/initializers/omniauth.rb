b07df962d7efe3a05f2964d8a4f0b8860e9745d3
Hooopo Wang <hoooopo@gmail.com>
2020-08-21 17:48:38 +0800

    access github user:email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
+  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "read:user, user:email"
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
 end
