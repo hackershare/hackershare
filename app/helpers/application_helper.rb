4bca6bbde300a467c8a93da2f41fd3031fc1ea0f
Hooopo Wang <hoooopo@gmail.com>
2020-08-28 05:52:41 +0800

    add link to active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,24 @@ module ApplicationHelper
     end
     link_to text, url, options
   end
+
+  def filter_link_to_active(text, url, current = :smart, options = {})
+    if options[:class]
+      if (params[:dt].to_s == current.to_s) || (params[:dt].blank? && current.to_s == 'smart')
+        options[:class] = options[:class] << " bg-gray-200"
+      end
+    end
+    link_to text, url, options
+  end
+
+  def hacker_link_to_active(text, url, current = :created, options = {})
+    if options[:class]
+      if (params[:type].to_s == current.to_s) || (params[:type].blank? && current.to_s == 'created')
+        options[:class] = options[:class] << " bg-gray-200"
+      end
+    end
+    link_to text, url, options
+  end
+
+
 end
