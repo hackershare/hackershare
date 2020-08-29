07f5c8e26c2757d5a7bbdc74f11c3a05d2eaa24e
Hooopo Wang <hoooopo@gmail.com>
2020-08-30 02:41:05 +0800

    fix broken url

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -5,6 +5,6 @@ class ExtensionsController < ApplicationController
 
   def create
     bookmark = current_user.bookmarks.create(url: params[:url])
-    render json: bookmark.reload.attributes.merge(location: Rails.application.routes.url_helpers.bookmark_url(bookmark)).to_json
+    render json: bookmark.reload.attributes.merge(location: Rails.application.routes.url_helpers.bookmark_url(nil, bookmark)).to_json
   end
 end
