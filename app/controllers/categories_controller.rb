6d42b2faf9c8736d9da5b3006b25f5fde2b07825
Hooopo Wang <hoooopo@gmail.com>
2020-09-04 07:31:17 +0800

    tag list page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,17 @@
 
 class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @pagy, @tags = pagy_countless(
+      Tag.preload(:user).order(bookmarks_count: :desc),
+      items: 12,
+      link_extra: 'data-remote="true" data-action="ajax:success->listing#replace"'
+    )
+
+    respond_to do |format|
+      format.js { render partial: "categories/list", content_type: "text/html" }
+      format.html
+    end
+  end
 end
