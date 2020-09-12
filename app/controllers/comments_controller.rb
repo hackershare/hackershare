9ee8931ce93e4a2fd780616aa04a9887a2ac4f94
Hooopo Wang <hoooopo@gmail.com>
2020-09-13 00:12:50 +0800

    fix notification to self

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     @bookmark = Bookmark.find(params[:bookmark_id])
     @comment = @bookmark.comments.new(comment: params[:comment], user: current_user)
     if @comment.save
-      CommentNotification.with(comment: @comment).deliver([@bookmark.user, @bookmark.comments.map { |x| x.user }].flatten.uniq)
+      CommentNotification.with(comment: @comment).deliver([@bookmark.user, @bookmark.comments.map { |x| x.user }].flatten.uniq - [current_user])
       respond_to do |format|
         format.js { render @comment, content_type: "text/html" }
         format.html do
