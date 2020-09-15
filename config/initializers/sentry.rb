2639a7f07b3a3cb96e5d30520f2ead1fb6bbb314
Hooopo Wang <hoooopo@gmail.com>
2020-09-15 22:34:41 +0800

    setup sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Raven.configure do |config|
+  config.dsn = ENV["SENTRY_DSN"] || "https://318730ee61334b8d926596efb3f9b0ba@o82609.ingest.sentry.io/5429716"
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+end
