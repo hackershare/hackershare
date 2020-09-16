aa6b83e5a672df962c3e7db3a0b884c0445c69b9
Song Huang <songhuangcn@qq.com>
2020-09-16 22:38:22 +0800

    Add sidekiq authorization and change robot job queue

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,18 @@
 require "sidekiq/web"
 require "sidekiq-scheduler/web"
 
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
+end
+
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
 
