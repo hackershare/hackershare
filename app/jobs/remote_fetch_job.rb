83350745f386a0719012bbaf7fc502623ed903ce
Hooopo Wang <hoooopo@gmail.com>
2020-09-16 09:43:11 +0800

    done

--- app/jobs/remote_fetch_job.rb
+++ app/jobs/remote_fetch_job.rb
@@ -16,13 +16,13 @@ class RemoteFetchJob < ApplicationJob
       favicon_url = [url_parser.scheme, "://", url_parser.host, "/favicon.ico"].join
       begin
         result = open(favicon_url, open_timeout: 8)
-        favicon_url = nil if result.content_type =~ /html|text/i
+        favicon_url = nil if /html|text/i.match?(result.content_type)
       rescue OpenURI::HTTPError, Errno::ENOENT
         favicon_url = nil
       end
     end
     lang = [parser.title, parser.description].any? { |text| text.to_s.match?(/\p{Han}/) } ? :chinese : :english
-    bookmark.update({favicon: favicon_url, description: parser.description, title: parser.title, lang: lang}.compact)
+    bookmark.update({ favicon: favicon_url, description: parser.description, title: parser.title, lang: lang }.compact)
     bookmark.save_favicon if bookmark.favicon.present?
   rescue LinkThumbnailer::HTTPError
     # TODO
