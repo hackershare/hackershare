062b6b919f32fc1a3c2380e436cc9c289f0ae1de
Hooopo Wang <hoooopo@gmail.com>
2020-09-16 14:43:34 +0800

    add unsubscribe link for email notification

--- test/mailers/previews/notifications/comment_mailer_preview.rb
+++ test/mailers/previews/notifications/comment_mailer_preview.rb
@@ -2,4 +2,11 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/notifications/comment_mailer
 class Notifications::CommentMailerPreview < ActionMailer::Preview
+  def notify
+    Notifications::CommentMailer.with(
+      comment: Comment.last,
+      recipient: User.first,
+      record: Notification.last
+    ).notify
+  end
 end
