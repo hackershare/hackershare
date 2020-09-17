feae4cc5b2d0b1221abaca08fe57b5b181b72356
Song Huang <songhuangcn@qq.com>
2020-09-17 22:19:31 +0800

    Robot job for Dribbble Popular

--- db/fixtures/rss_sources.rb
+++ db/fixtures/rss_sources.rb
@@ -1,7 +1,8 @@
 RssSource.seed(:id,
-  { id: 1, code: "meituan_tech",    url: "https://tech.meituan.com/feed/",           name: '美团技术',          limit: nil },
-  { id: 2, code: "hackernews_best", url: "https://rsshub.app/hackernews/best",       name: 'Hacker News Best', limit: 1   },
-  { id: 3, code: "manong_weekly",   url: "https://rsshub.app/manong-weekly",         name: '码农周刊',          limit: 1   },
-  { id: 4, code: "github_trending", url: "https://rsshub.app/github/trending/daily", name: 'GitHub Trending',  limit: 1   },
-  { id: 5, code: "wanqu",           url: "https://wanqu.co/feed/",                   name: '湾区日报',          limit: nil  },
+  { id: 1, code: "meituan_tech",     url: "https://tech.meituan.com/feed/",           name: "美团技术",          limit: nil },
+  { id: 2, code: "hackernews_best",  url: "https://rsshub.app/hackernews/best",       name: "Hacker News Best", limit: 1   },
+  { id: 3, code: "manong_weekly",    url: "https://rsshub.app/manong-weekly",         name: "码农周刊",          limit: 1   },
+  { id: 4, code: "github_trending",  url: "https://rsshub.app/github/trending/daily", name: "GitHub Trending",  limit: 1   },
+  { id: 5, code: "wanqu",            url: "https://wanqu.co/feed/",                   name: "湾区日报",          limit: nil  },
+  { id: 6, code: "dribbble_popular", url: "https://rsshub.app/dribbble/popular/week", name: "Dribbble Popular", limit: nil  },
 )
