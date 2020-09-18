9874a8259a382fce8c5b9b7a5cd791c9374e74c1
Hooopo Wang <hoooopo@gmail.com>
2020-09-18 21:55:35 +0800

    use helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -84,7 +84,7 @@ module ApplicationHelper
 
   def render_rss_icon(tag)
     icon = <<~ICON
-      <svg class="ml-1.5 h-4 w-4 text-indigo-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg class="ml-2 h-4 w-4 text-indigo-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
       </svg>
     ICON
