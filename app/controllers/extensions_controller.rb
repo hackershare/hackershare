2a2bc92a10b0abac7eef01cc277bcef87c1f5a24
Hooopo Wang <hoooopo@gmail.com>
2020-09-19 17:23:40 +0800

    fix chrome extension notification

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -5,6 +5,7 @@ class ExtensionsController < ApplicationController
 
   def create
     bookmark = current_user.bookmarks.create(url: params[:url])
+    UserFeedNotification.with(bookmark: bookmark).deliver(current_user.follower_users)
     render json: bookmark.reload.attributes.merge(location: Rails.application.routes.url_helpers.bookmark_url(nil, bookmark)).to_json
   end
 end
