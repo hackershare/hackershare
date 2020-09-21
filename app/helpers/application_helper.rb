8cdaed3005093b8ff0f47068bb7164af55e6be5b
bindiry <bindiry@gmail.com>
2020-09-21 23:37:28 +0700

    set a title for the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def title(text)
+    content_for :title, text
+  end
+
   def disable_with_spinner(text)
     spinner = <<~SPINNER
       <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
