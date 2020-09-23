08ee28bbc5721c87813d192c0a145fc24fc6359b
Song Huang <songhuangcn@qq.com>
2020-09-23 21:13:44 +0800

    use common request for commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     if @comment.save
       CommentNotification.with(comment: @comment).deliver([@bookmark.user, @bookmark.comments.map { |x| x.user }].flatten.uniq - [current_user])
       respond_to do |format|
-        format.js { render @comment, content_type: "text/html" }
+        # format.js { render @comment, content_type: "text/html" }
         format.html do
           flash[:success] = t("comment_added")
           redirect_to bookmark_path(@bookmark)
