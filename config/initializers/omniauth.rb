18217198a3116cdd1089fa88e8febd214d24d598
QuantumGhost <obelisk.reg+github@gmail.com>
2020-09-23 21:18:32 +0800

    Remove unnecessary scopes
    I have tried to login to hackershare.dev without these scopes and found no issue. So I guess these scopes are not really necessary.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "read:user, user:email"
+  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
 end
