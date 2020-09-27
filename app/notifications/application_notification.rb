3a7c199e09643fef1aa2fd3066ce07ff6342b74e
Song Huang <songhuangcn@qq.com>
2020-09-28 02:08:58 +0800

    fix i18n problem

--- app/notifications/application_notification.rb
+++ app/notifications/application_notification.rb
@@ -5,7 +5,7 @@ class ApplicationNotification < Noticed::Base
 
     # Cannot use this this method until I18n.locale is already setted
     def default_url_options
-      locale = I18n.locale == I18n.default_locale ? nil : I18n.locale
+      locale = I18n.locale == I18n.default_locale ? nil : "cn"
       {
         locale: locale,
       }
