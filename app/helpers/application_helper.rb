4e6ce764e6cc3c8a605b7859b0f8cecf809dfbbc
Song Huang <songhuangcn@qq.com>
2020-10-01 21:16:26 +0800

    fix sharable url #80

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -79,7 +79,7 @@ module ApplicationHelper
         concat render_rss_icon(tag)
       end
     else
-      link_to pretty_bookmarks_path(request.params.except(:page).merge(tag: tag.name)), class: "btn-tag" do
+      link_to bookmarks_path(request.params.except(:page).merge(tag: tag.name)), class: "btn-tag" do
         concat "##{tag.name}"
         concat render_rss_icon(tag)
       end
