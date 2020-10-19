c78837caa27be4bffe26bbb1c045e8c8f40ecb9b
Hooopo Wang <hoooopo@gmail.com>
2020-10-20 06:26:21 +0800

    add opengraph

--- db/structure.sql
+++ db/structure.sql
@@ -359,7 +359,8 @@ CREATE TABLE public.bookmarks (
     is_display boolean DEFAULT true NOT NULL,
     tsv tsvector GENERATED ALWAYS AS (((((setweight(to_tsvector('public.zh'::regconfig, (COALESCE(title, ''::character varying))::text), 'A'::"char") || setweight(to_tsvector('public.zh'::regconfig, (COALESCE(url, ''::character varying))::text), 'A'::"char")) || setweight(to_tsvector('public.zh'::regconfig, (COALESCE(cached_tag_with_aliases_names, ''::character varying))::text), 'A'::"char")) || setweight(to_tsvector('public.zh'::regconfig, COALESCE(description, ''::text)), 'B'::"char")) || setweight(to_tsvector('public.zh'::regconfig, COALESCE(content, ''::text)), 'D'::"char"))) STORED,
     shared_at timestamp without time zone,
-    pinned_comment_id bigint
+    pinned_comment_id bigint,
+    images character varying[] DEFAULT '{}'::character varying[]
 );
 
 
@@ -1361,6 +1362,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20201010113850'),
 ('20201012190758'),
 ('20201019145723'),
-('20201019155740');
+('20201019155740'),
+('20201019214125');
 
 
