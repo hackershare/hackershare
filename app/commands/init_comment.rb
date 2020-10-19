6d6c2216ac257f10734253128d5b36fb2f39df3d
Hooopo Wang <hoooopo@gmail.com>
2020-10-20 00:32:12 +0800

    fix long text

--- app/commands/init_comment.rb
+++ app/commands/init_comment.rb
@@ -14,6 +14,8 @@ class InitComment
     return if bookmark.ref_id.present?
     return if bookmark.comments_count >= 1
     return if bookmark.description.blank?
+    return if bookmark.description.to_s.size > 280
+    return if /[<>]/.match?(bookmark.description.to_s)
     pinned_comment_text = bookmark.description
     pinned_comment = bookmark.comments.create(user: User.rss_robot, comment: pinned_comment_text)
     bookmark.update(pinned_comment: pinned_comment)
