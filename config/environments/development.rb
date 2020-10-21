e43de38d8edd7ab7b3a3364365a0ee40312dbac9
Hooopo Wang <hoooopo@gmail.com>
2020-10-21 23:57:20 +0800

    oneline log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+  config.active_record.verbose_query_logs = false
+  config.log_level = :debug
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
