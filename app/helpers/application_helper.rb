98e41dea74f5489536d6da209ec7386522e981b3
Song Huang <songhuangcn@qq.com>
2020-10-22 20:31:12 +0800

    fix tag margin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,12 +78,12 @@ module ApplicationHelper
 
   def tag_url_for(tag)
     if controller_name == "users"
-      link_to bookmarks_path(request.query_parameters.except(:page).merge(tag: tag.name, only_path: true)), class: "btn-tag", data: { remote: true, action: "ajax:success->listing#replace" } do
+      link_to bookmarks_path(request.query_parameters.except(:page).merge(tag: tag.name, only_path: true)), class: "btn-tag mr-1 lg:mr-2 mb-1", data: { remote: true, action: "ajax:success->listing#replace" } do
         concat "##{tag.name}"
         concat render_rss_icon(tag)
       end
     else
-      link_to bookmarks_path(request.query_parameters.except(:page).merge(tag: tag.name)), class: "btn-tag" do
+      link_to bookmarks_path(request.query_parameters.except(:page).merge(tag: tag.name)), class: "btn-tag mr-1 lg:mr-2 mb-1" do
         concat "##{tag.name}"
         concat render_rss_icon(tag)
       end
