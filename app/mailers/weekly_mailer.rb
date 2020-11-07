81215994f1731a3cb80cdc517771e42782e7fb9d
Song Huang <songhuangcn@qq.com>
2020-11-07 20:27:35 +0800

    fix weekly mailer params

--- app/mailers/weekly_mailer.rb
+++ app/mailers/weekly_mailer.rb
@@ -4,7 +4,7 @@ class WeeklyMailer < ApplicationMailer
   def notify
     user = params[:user]
     @weekly_selection = params[:weekly_selection]
-    if params[:preview] || email_notifications?(@user)
+    if params[:preview] || email_notifications?(user)
       mail(to: user.email, subject: @weekly_selection.full_title)
     end
   end
