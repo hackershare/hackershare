fa23788fa25cbe33cd7bfe18c9b15e964903180c
Hooopo Wang <hoooopo@gmail.com>
2022-03-07 23:59:28 +0800

    rm ahoy

--- app/javascript/hackershare/index.js
+++ app/javascript/hackershare/index.js
@@ -9,9 +9,6 @@ import { createPopper } from '@popperjs/core';
 import { format, render, cancel, register } from 'timeago.js';
 import Tagify from '@yaireo/tagify';
 
-import ahoy from "ahoy.js";
-ahoy.trackAll();
-
 //import TubrolinksPrefetch from 'turbolinks-prefetch'
 //TubrolinksPrefetch.start()
 
--- config/initializers/ahoy.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Ahoy::Store < Ahoy::DatabaseStore
-end
-
-# set to true for JavaScript tracking
-Ahoy.api = true
